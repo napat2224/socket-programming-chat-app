d26898b005cbeaff1aad860ace39dc6b414fd9f7
wnmay <wanathachat@gmail.com>
2025-11-17 05:15:28 +0700

    feat: fetch room data and real-time member list

--- frontend/src/components/chat/memberList.tsx
+++ frontend/src/components/chat/memberList.tsx
@@ -1,36 +1,37 @@
-"use clients";
+"use client";
 
-import { avatars } from "@/types/avatar";
 import Image from "next/image";
+import { avatars } from "@/types/avatar";
 
 export interface MemberProps {
-    id: string;
-    profile: number;
-    name: string;
+  id: string;
+  profile: number;
+  name: string;
 }
 
-export default function MemberList({
-    memberList,
-    className,
-}:{ memberList:  MemberProps[]; className?: string}) {
+interface MemberListProps {
+  memberList: MemberProps[];
+  className?: string;
+}
 
-    return (
-        <div className={`w-full flex overflow-x-scroll scroll-m-0 p-2 gap-4 ${className}`}>
-            {memberList.map((m) => (
-                <div 
-                    key={m.id}
-                    className="flex flex-col items-center justify-center">
-                    <div className="w-7 h-7 rounded-full overflow-hidden relative">
-                       <Image 
-                            src={avatars[m.profile]}
-                            alt={m.name}
-                            fill
-                            className="object-cover"
-                        /> 
-                    </div>
-                    <span className="text-xs mt-1 font-semibold">{m.name}</span>
-                </div>
-            ))}
+export default function MemberList({ memberList, className }: MemberListProps) {
+  return (
+    <div
+      className={`w-full flex overflow-x-scroll scroll-m-0 p-2 gap-4 ${className}`}
+    >
+      {memberList.map((m) => (
+        <div key={m.id} className="flex flex-col items-center justify-center">
+          <div className="w-7 h-7 rounded-full overflow-hidden relative">
+            <Image
+              src={avatars[m.profile]}
+              alt={m.name}
+              fill
+              className="object-cover"
+            />
+          </div>
+          <span className="text-xs mt-1 font-semibold">{m.name}</span>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
